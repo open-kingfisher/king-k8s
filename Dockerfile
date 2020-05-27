9ce2d99e59dae0a8e7732c23992771d180aa8b67
kr521 <macao450@163.com>
2020-05-27 11:15:22 +0800

    修改Dockerfile编译方式

--- Dockerfile
+++ Dockerfile
@@ -10,15 +10,6 @@ RUN set -xe \
 FROM alpine:3.10
 
 ARG NAME="king-k8s"
-ENV TIME_ZONE Asia/Shanghai
-RUN set -xe \
-    && sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-    && apk --no-cache add --update ca-certificates && apk add --no-cache tzdata \
-    && echo "${TIME_ZONE}" > /etc/timezone \
-    && ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime \
-    && mkdir /lib64 \
-    && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2 
-
 COPY --from=builder /$NAME/entrypoint.sh /entrypoint.sh
 COPY --from=builder /$NAME/bin/$NAME /usr/local/bin
 COPY --from=builder /$NAME/bin/$NAME-grpc /usr/local/bin
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,17 @@
 [ "$PORT" ] || PORT=8080
 [ "$RPCPORT" ] || RPCPORT=50000
 [ "$RABBITMQ_URL" ] || RABBITMQ_URL='amqp://user:password@king-rabbitmq:5672/'
+[ "$TIME_ZONE" ] || TIME_ZONE="Asia/Shanghai"
+[ "$ALPINE_REPO" ] || ALPINE_REPO="mirrors.aliyun.com"
 
-mkdir /var/log/kingfisher
+sed -i 's/dl-cdn.alpinelinux.org/${ALPINE_REPO}/g' /etc/apk/repositories     
+apk --no-cache add tzdata 
+echo "${TIME_ZONE}" > /etc/timezone 
+ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime 
+mkdir /lib64 
+ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+
+mkdir -p /var/log/kingfisher
 
 # grpc
 /usr/local/bin/king-k8s-grpc -dbURL=$DB_URL -listen=$LISTEN:$PORT -listen=$LISTEN:$RPCPORT &
