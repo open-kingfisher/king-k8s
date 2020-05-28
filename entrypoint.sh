40894d33c0278f9a126baeea10dd6442c90832a6
kr521 <macao450@163.com>
2020-05-28 17:40:21 +0800

    修复entrypoint.sh文件错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
-[ "$DB_URL" ] || DB_URL='user:password@tcp(192.168.10.100:3306)/kingfisher'
+
+[ "$MYSQL_USER" ] || MYSQL_USER='root'
+[ "$MYSQL_PASSWORD" ] || MYSQL_PASSWORD='kingfisher'
+[ "$MYSQL_HOST" ] || MYSQL_HOST='mysql'
+[ "$MYSQL_PORT" ] || PORT="3306"
+[ "$MYSQL_DB" ] || DB="kingfisher"
+[ "$DB_URL" ] || DB_URL="${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DB}"
+
+[ "$MQ_USER" ] || MQ_USER=kingfisher
+[ "$MQ_PASSWORD" ] || MQ_PASSWORD=kingfisher
+[ "$MQ_HOST" ] || MQ_HOST=king-rabbitmq
+[ "$MQ_PORT" ] || MQ_PORT=5672
+[ "$RABBITMQ_URL" ] || RABBITMQ_URL="amqp://${MQ_USER}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}/"
+
 [ "$LISTEN" ] || LISTEN=0.0.0.0
 [ "$PORT" ] || PORT=8080
 [ "$RPCPORT" ] || RPCPORT=50000
-[ "$RABBITMQ_URL" ] || RABBITMQ_URL='amqp://user:password@king-rabbitmq:5672/'
 [ "$TIME_ZONE" ] || TIME_ZONE="Asia/Shanghai"
 [ "$ALPINE_REPO" ] || ALPINE_REPO="mirrors.aliyun.com"
 
-sed -i 's/dl-cdn.alpinelinux.org/${ALPINE_REPO}/g' /etc/apk/repositories     
+sed -i "s/dl-cdn.alpinelinux.org/${ALPINE_REPO}/g" /etc/apk/repositories     
 apk --no-cache add tzdata 
 echo "${TIME_ZONE}" > /etc/timezone 
 ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime 
